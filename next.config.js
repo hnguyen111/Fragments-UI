7d4f26a524af9744c54f5212f94a34a1a890ea55
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-07-02 14:54:13 -0400

    Removed the internal environment variables from the UI server

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_URL: "http://ec2-54-83-72-6.compute-1.amazonaws.com:8080",
-    AWS_REGION: "us-east-1",
-    AWS_COGNITO_POOL_ID: "us-east-1_Gv1jSjfzy",
-    AWS_COGNITO_APP_CLIENT_ID: "1cccuq4c8dit4f0b1k7kv9s6ir",
-  },
   experimental: {
     outputStandalone: true,
   },
